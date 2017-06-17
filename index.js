7620ed7447269eaf0e2ef3e833bc303c0232aea9
john bauer <johnbauer12693@gmail.com>
2017-06-17 10:50:17 -0400

    watson integration

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 /*===============================
         EXPRESS SETUP
 ================================*/
-
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const env = require('dotenv').config();
 
+/*==============================
+          WATSON
+===============================*/
+const watson = require('watson-developer-cloud');
+const language_translator = watson.language_translator({
+  url: "https://gateway.watsonplatform.net/language-translator/api",
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  version: "v2"
+});
+
+translate = (msg) => {
+  console.log('translating');
+  language_translator.translate({
+    text: msg,
+    source: "en",
+    target: "es"
+  }, function(err, translation) {
+    if (err){
+      output = "error";
+      console.log(output);
+      return output;
+    }
+    else {
+      let output = (translation.translations[0].translation);
+      console.log(output);
+      return output;
+    }
+  });
+}
+
+/*================================
+          ROUTES
+================================*/
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+/*================================
+          CHAT SERVER
+================================*/
 
 /*------ log "a user connected"-------*/
 io.on('connection', function(socket){
@@ -20,19 +57,13 @@ io.on('connection', function(socket){
   })
 });
 
-/*----- print chat message event -----*/
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-
 /*------- emit message to channel ---*/
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
+
 /*------ listen on 8080 ------*/
 http.listen(8080, function(){
   console.log('listening on port 8080');
